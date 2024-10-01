8f83fca34582bf954f2a71d3b12f7bf28e8a7dc1
Laura Schoerken <lauraschorken@gmail.com>
2024-10-01 18:15:59 +0200

    feat: expansionChecker

---
+++ wow/src/main/java/com/wow_database/wow/model/enums/checker/ExpansionChecker.java
@@ -0,0 +1,22 @@
+package com.wow_database.wow.model.enums.checker;
+
+import com.wow_database.wow.model.enums.Expansion;
+
+public class ExpansionChecker {
+
+	private ExpansionChecker() {
+	}
+
+	public static String checkExpansion(String input) {
+		if (input == null || input.trim().isEmpty()) {
+			return null;
+		}
+		try {
+			Expansion expansion = Expansion.valueOf(input.toUpperCase().trim());
+			return expansion.name(); // Retornar el nombre de la expansión
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(input + " no es una expansión válida.", e);
+		}
+	}
+
+}
