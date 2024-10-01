1b6944225a2be263fb41363e43eade2a5525e0cb
Laura Schoerken <lauraschorken@gmail.com>
2024-10-01 18:48:32 +0200

    feat: add opctions to error message

--- wow/src/main/java/com/wow_database/wow/global/handler/GlobalExceptionHandler.java
+++ wow/src/main/java/com/wow_database/wow/global/handler/GlobalExceptionHandler.java
@@ -6,41 +6,64 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.wow_database.wow.model.enums.ClassName;
+import com.wow_database.wow.model.enums.Difficulty;
+import com.wow_database.wow.model.enums.Expansion;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private String error = "Error: ";
 
-	// Manejador para errores de Jackson (JSON no válido)
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	public ResponseEntity<CustomErrorResponse> handleHttpMessageNotReadableException(
 			HttpMessageNotReadableException e) {
-		// Mensaje y código de error personalizado
-		String errorMessage = "Error: " + e.getMessage();
-		String errorCode = "JSON_PARSE_ERROR"; // Código de error específico
+		String errorMessage = error + e.getMessage();
+		String errorCode = "JSON_PARSE_ERROR";
 
 		CustomErrorResponse errorResponse = new CustomErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage,
-				errorCode);
+				errorCode, null);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 
-	// Manejo de IllegalArgumentException (u otra excepción específica)
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<CustomErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-		String errorMessage = "Error: " + e.getMessage();
-		String errorCode = "INVALID_ARGUMENT"; // Código de error para argumentos inválidos
+		String errorMessage = error + e.getMessage();
+		String errorCode = "INVALID_ARGUMENT";
 
+		StringBuilder options = new StringBuilder();
+
+		if (e.getMessage().contains("expansion")) {
+			for (Expansion exp : Expansion.values()) {
+				options.append(exp.name()).append(", ");
+			}
+		} else if (e.getMessage().contains("class")) {
+			for (ClassName cls : ClassName.values()) {
+				options.append(cls.name()).append(", ");
+			}
+		} else if (e.getMessage().contains("difficulty")) {
+			for (Difficulty difficulty : Difficulty.values()) {
+				options.append(difficulty.name()).append(", ");
+			}
+		} else {
+			options.append("No hay opciones disponibles.");
+		}
+
+		if (options.length() > 0) {
+			options.setLength(options.length() - 2);
+		}
 		CustomErrorResponse errorResponse = new CustomErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage,
-				errorCode);
+				errorCode, options.toString());
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 
-	// Manejo de excepción general (Exception)
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<CustomErrorResponse> handleGeneralException(Exception e) {
-		String errorMessage = "Error: " + e.getMessage();
-		String errorCode = "GENERIC_ERROR"; // Código de error genérico
+		String errorMessage = error + e.getMessage();
+		String errorCode = "GENERIC_ERROR";
 
 		CustomErrorResponse errorResponse = new CustomErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-				errorMessage, errorCode);
+				errorMessage, errorCode, null);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 	}
 }
