3aa6236ec6959de9847ecb538cf9cda628e5498b
Laura Schoerken <lauraschorken@gmail.com>
2024-10-01 18:48:04 +0200

    feat: add options to customErrorResponse

--- wow/src/main/java/com/wow_database/wow/global/handler/CustomErrorResponse.java
+++ wow/src/main/java/com/wow_database/wow/global/handler/CustomErrorResponse.java
@@ -11,35 +11,5 @@ public class CustomErrorResponse {
 	private int status;
 	private String message;
 	private String errorCode;
-
-	public CustomErrorResponse(int status, String message) {
-		this.status = status;
-		this.message = message;
-	}
-
-	// Getters y Setters
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getErrorCode() {
-		return errorCode;
-	}
-
-	public void setErrorCode(String errorCode) {
-		this.errorCode = errorCode;
-	}
+	private String options;
 }
