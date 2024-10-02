2b630b65770b5a839dfa6ff48e8fb4fc736d0dc7
Laura Schoerken <lauraschorken@gmail.com>
2024-10-02 13:31:27 +0200

    feat: added loader for the files

--- wow/src/main/java/com/wow_database/wow/WowApplication.java
+++ wow/src/main/java/com/wow_database/wow/WowApplication.java
@@ -3,13 +3,14 @@ package com.wow_database.wow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wow_database.wow.global.file.FileManager;
+
 @SpringBootApplication
 public class WowApplication {
 
 	public static void main(String[] args) {
+		FileManager.loadTexts();
 		SpringApplication.run(WowApplication.class, args);
 	}
-	
-	
 
 }
