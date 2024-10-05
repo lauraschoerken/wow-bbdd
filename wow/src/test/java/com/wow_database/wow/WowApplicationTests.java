3e1f495e4303de5b4c644db96b69e22b3cd38a5d
Laura Schoerken <lauraschorken@gmail.com>
2024-10-05 16:02:50 +0200

    feat: general test settings

--- wow/src/test/java/com/wow_database/wow/WowApplicationTests.java
+++ wow/src/test/java/com/wow_database/wow/WowApplicationTests.java
@@ -3,11 +3,15 @@ package com.wow_database.wow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.wow_database.wow.global.file.FileManager;
+
 @SpringBootTest
 class WowApplicationTests {
 
 	@Test
 	void contextLoads() {
+		FileManager.loadTexts();
+
 	}
 
 }
